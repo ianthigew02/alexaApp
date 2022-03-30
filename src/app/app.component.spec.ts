af2943ad6ec1754ea37180e170a2ff2c754cf71e
christospapadopoulos54 <christospapadopoulos54@hotmail.com>
2022-03-30 22:51:28 +0300

    register and login

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'alexaApp1'`, () => {
+  it(`should have as title 'alexaApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('alexaApp1');
+    expect(app.title).toEqual('alexaApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('alexaApp1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('alexaApp app is running!');
   });
 });
